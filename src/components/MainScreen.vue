4b78458bf7459ba2e39c0a1f28acab98a8c0fee6
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-15 16:12:45 +0700

    modified and add new

---
+++ src/components/MainScreen.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="main-screen">
+    <div class="container">
+      <div class="inner">
+        <div class="answered">
+          <div class="question">
+            <h2>1. Enter Question Here</h2>
+          </div>
+          <div class="answers">
+            <div class="answer-item">
+              <label for="a">A</label>
+              <input type="radio" v-model="answer" />
+            </div>
+            <div class="answer-item">
+              <label for="a">A</label>
+              <input type="radio" v-model="answer" />
+            </div>
+
+            <div class="answer-item">
+              <label for="a">A</label>
+              <input type="radio" v-model="answer" />
+            </div>
+
+            <div class="answer-item">
+              <label for="a">A</label>
+              <input type="radio" v-model="answer" />
+            </div>
+          </div>
+        </div>
+        <div class="scored">1</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const answer = ref(null);
+</script>
+
+<style lang="scss" scoped>
+.main-screen {
+  background-color: #303030;
+}
+
+.inner {
+  display: grid;
+  grid-template-columns: 1fr 0.2fr;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  gap: 40px;
+
+  .answered {
+    background: yellow;
+  }
+
+  .scored {
+    background-color: #fff;
+  }
+
+  .question {
+    border: 3px solid #fff;
+  }
+
+  .answers {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 30px;
+
+    .answer-item {
+      border: 1px solid red;
+      border-radius: 8px;
+
+      label {
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="welcome-screen">
+    <div class="container bg-welcome">
+      <div class="content">
+        <div class="logo">
+          <img src="../assets/images/logo.png" alt="Logo" />
+        </div>
+
+        <button @click="onStart">START</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(["start"]);
+function onStart() {
+  emit("start");
+}
+</script>
+
+<style lang="scss" scoped>
+.welcome-screen {
+  background: #805850;
+  min-height: 100vh;
+}
+
+.bg-welcome {
+  background: url("../assets/images/bg-1.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: auto;
+  height: 100vh;
+  text-align: center;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 50px;
+  height: 100%;
+}
+
+.logo {
+  text-align: center;
+}
+
+button {
+  display: inline-block;
+  padding: 7px 14px;
+  border-radius: 8px;
+  border: 1px solid #fff;
+  background-color: #000;
+  color: #fff;
+  font-size: 2.25rem;
+  transition: all 0.3s linear;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
