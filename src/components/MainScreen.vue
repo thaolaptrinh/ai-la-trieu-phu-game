907b051a80095f0a0dd6cb28054060d0ccc2dc56
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-05-31 13:50:42 +0700

    modified

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -13,8 +13,16 @@
       </div>
     </div>
     <div class="prizeList">
-      <ul class="moneyList">
-        1
+      <ul className="moneyList">
+        <li
+          v-for="money in prizeList"
+          :key="money.id"
+          class="moneyListItem"
+          :class="{ active: questionNumber === money.id }"
+        >
+          <span className="moneyListItemNumber">{{ money.id }}</span>
+          <span className="moneyListItemAmount">{{ money.amount }}</span>
+        </li>
       </ul>
     </div>
   </div>
@@ -22,6 +30,32 @@
 
 <script setup>
 import { ref } from "vue";
+const questionNumber = ref(1);
+const stop = ref(false);
+const earned = ref("$ 0");
+const prizeList = [
+  { id: 1, amount: "$ 100" },
+  { id: 2, amount: "$ 200" },
+  { id: 3, amount: "$ 300" },
+  { id: 4, amount: "$ 500" },
+  { id: 5, amount: "$ 1.000" },
+  { id: 6, amount: "$ 2.000" },
+  { id: 7, amount: "$ 4.000" },
+  { id: 8, amount: "$ 8.000" },
+  { id: 9, amount: "$ 16.000" },
+  { id: 10, amount: "$ 32.000" },
+  { id: 11, amount: "$ 64.000" },
+  { id: 12, amount: "$ 125.000" },
+  { id: 13, amount: "$ 250.000" },
+  { id: 14, amount: "$ 500.000" },
+  { id: 15, amount: "$ 1.000.000" },
+].reverse();
+
+const playAgain = () => {
+  setQuestionNumber(1);
+  setStop(false);
+  setEarned("$ 0");
+};
 
 const answer = ref(null);
 </script>
